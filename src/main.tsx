cba8c005e1a8632415908e8c4e24f714838fda31
Thanh Tùng <thanhtung088@gmail.com>
2026-01-19 10:46:16 +0700

    Fix: Add Tailwind directives and import CSS to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css' // Dòng này cực kỳ quan trọng
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
