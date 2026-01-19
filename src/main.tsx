62edfac4924015f6cc02c7916c5f7a92b7152c85
Thanh Tùng <thanhtung088@gmail.com>
2026-01-19 10:35:14 +0700

    Confirm: main.tsx is added and index.html is updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// File này có nhiệm vụ gắn toàn bộ ứng dụng React vào thẻ <div id="root">
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
